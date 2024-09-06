536f2ecf89d51ca980ad4d84f038a3106358ef01
Kartik Chinda <kartik.chinda@gmail.com>
2024-09-07 00:28:21 +0530

    Made UIchanges to the moreOptions tab.

--- src/components/RangePicker/MoreOptions.tsx
+++ src/components/RangePicker/MoreOptions.tsx
@@ -11,16 +11,16 @@ const MoreOptions = ({ isCalendarVisible, setisCalendarVisible }: moreOptionProp
     }
 
     return (
-        <div className='w-full flex justify-between  items-center bg-black rounded-xl h-16'>
-            <div>
-                <button className='more-options-button  ml-4'>Last 7 days</button>
-                <button className='more-options-button mx-1'>Last 30 days</button>
+        <div className='w-full flex flex-row   justify-around md:justify-between items-center bg-black rounded-xl md:px-2'>
+            <div className="more-options-container ">
+                <button className='more-options-button '>Last 7 days</button>
+                <button className='more-options-button  '>Last 30 days</button>
             </div>
-            <div>
-                <button onClick={handleCancelClick} className='more-options-button mx-1 hover:scale-[0.8]'>
+            <div className="more-options-container ">
+                <button onClick={handleCancelClick} className='more-options-button  hover:scale-[0.8] '>
                     Cancel
                 </button>
-                <button className='more-options-button mr-4  hover:scale-105 font-black'>
+                <button className='more-options-button  hover:scale-105 font-black'>
                     Submit
                 </button>
             </div>
