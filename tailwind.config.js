d7ad9209517a5576300f60c44698880da7d3e00e
Kartik Chinda <kartik.chinda@gmail.com>
2024-09-06 13:15:17 +0530

    Added the color palette and font styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headings: "Bebas Neue",
+        text: "Lora",
+      },
+      colors: {
+        yellowPalette: {
+          background: "#F7F7F7",
+          componentBackground: "#000000",
+          componentForeground: "#FFDB00",
+        },
+        purplePalette: {
+          background: "#F7F7F7",
+          purpleLight: "#D6C6F7",
+          purpleDark: "#DDB4FF",
+        }
+      },
+    },
   },
   plugins: [],
 }
